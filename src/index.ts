c8ef937bffe32c906dad1a3a7f3d52c1bed35578
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-08-22 18:07:30 +0200

    Correct linter errors

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
 import "./css/style.css";
 import displayScore from "./modules/display";
-import {postScore, score} from "./modules/interact-api";
+import { postScore, score } from "./modules/interact-api";
 
-const ScoreWrapper = document.getElementById('score-container') as HTMLDivElement;
-const ScoreForm = document.getElementById('score-form') as HTMLFormElement;
-const RefreshBtn = document.getElementById('refresh-btn') as HTMLButtonElement;
-const NameInput = document.getElementById('name-input') as HTMLInputElement;
+const ScoreWrapper = document.getElementById(
+  "score-container"
+) as HTMLDivElement;
+const ScoreForm = document.getElementById("score-form") as HTMLFormElement;
+const RefreshBtn = document.getElementById("refresh-btn") as HTMLButtonElement;
+const NameInput = document.getElementById("name-input") as HTMLInputElement;
 
+const URL =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/A9GgGr2ZTgKdQScdJRGi/scores/";
 
-const URL = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/A9GgGr2ZTgKdQScdJRGi/scores/";
+window.addEventListener("DOMContentLoaded", () => {
+  displayScore(URL, ScoreWrapper);
+});
 
-window.addEventListener('DOMContentLoaded', ()=> {
-    displayScore(URL, ScoreWrapper);
-})
+RefreshBtn.addEventListener("click", () => {
+  displayScore(URL, ScoreWrapper);
+});
 
-RefreshBtn.addEventListener('click', ()=> {
-    displayScore(URL, ScoreWrapper);
-} )
-
-ScoreForm.addEventListener('submit', async (e)=> {
+ScoreForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const Play:score = {
+  const Play: score = {
     user: NameInput.value,
-    score: ScoreForm.score.value
-  }
-
-  console.log(Play)
+    score: ScoreForm.score.value,
+  };
 
   await postScore(URL, Play);
-    ScoreForm.reset()
-} )
-
+  ScoreForm.reset();
+});
